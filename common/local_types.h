d98753208e1131ba13fa261b64baddc6a2c3ed4d
Chris Nicolls <cnicolls@inmusicbrands.com>
2026-07-31 17:24:31 -0400

    Add processor specific file

---
+++ common/local_types.h
@@ -0,0 +1,11 @@
+/*
+ * This file is licensed under the terms described in the LICENSE
+ * file located in the root directory of this source tree.
+ */
+
+#ifndef LOCAL_TYPES_H_
+#define LOCAL_TYPES_H_
+
+#include "stdint.h"
+
+#endif // LOCAL_TYPES_H_
